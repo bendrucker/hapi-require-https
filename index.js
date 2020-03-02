602cdec6d528a653b01b5e786ca7c90a332abf4d
Ben Drucker <bvdrucker@gmail.com>
2020-03-02 13:53:30 -0800

    hapi: require v19 (+ node v12)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
 
-var pkg = require('./package.json')
+const pkg = require('./package.json')
 
 module.exports = { register, name: pkg.name }
 
 function register (server, options) {
   server.ext('onRequest', function (request, h) {
-    var redirect = options.proxy !== false
+    const redirect = options.proxy !== false
       ? request.headers['x-forwarded-proto'] === 'http'
       : request.server.info.protocol === 'http'
-    var host = request.headers['x-forwarded-host'] || request.headers.host
+    const host = request.headers['x-forwarded-host'] || request.headers.host
 
     if (!redirect) return h.continue
     return h
-      .redirect('https://' + host + (request.url.path || request.url.pathname + request.url.search))
+      .redirect('https://' + host + request.url.pathname + request.url.search)
       .takeover()
       .code(301)
   })
